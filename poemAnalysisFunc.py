6e7dd62a4fcd82cdd3f5fbd34bb0e2c7116558ff
Kris Shaffer <kris.shaffer@gmail.com>
2015-06-09 08:40:48 -0600

    converted script to function: poemAnalysisFunc.py

--- poemAnalysisFunc.py
+++ poemAnalysisFunc.py
@@ -1,62 +1,16 @@
-# Python script for analyzing phonemic data from IPA-encoded poems
-
-# Copyright (C) 2015 Kris P. Shaffer
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import codecs
 import csv
 import numpy
 
-# main variables
 
-poemCorpus = ['NachtUndTraumeIPA.txt', 'WohinIPA.txt', 'DasWandernIPA.txt']
-ignore = [' ', ':']
-phonemeCategory = {
-    'a': 'open',
-    u'\u0061': 'open',
-    'e': 'closeMid',
-    u'\u025b': 'openMid',
-    u'\u0259': 'neutral',
-    'i': 'close',
-    'I': 'open',
-#    u'\u026a': 'close',
-    'o': 'closeMid',
-    u'\u0254': 'openMid',
-    u'\u00f8': 'closeMid',
-    u'\u0153': 'openMid',
-    'y': 'close',
-    u'\u0153': 'close',
-    'u': 'close',
-    u'\028a': 'close',
-}
-vowelTypes = ['close', 'closeMid', 'neutral', 'openMid', 'open']
-phonemeCategoryList = list(set(phonemeCategory.values()))
-ignoreDiphthongs = True
+# line-by-line analysis
 
-# Set threshold
-threshold = 2 # number of standard deviations
-# not using this yet:
-categoryThreshold = 2 # number of categories that must meet threshold
-
-# main script
-
-for poem in poemCorpus:
-    filename = poem
+def poemAnalysisByLine(filename, categories, categoryDict, ignoreDiphthongs, threshold):
+    ignore = [' ', ':']
+    phonemeCategoryList = list(set(phonemeCategory.values()))
     name = filename.split('.')[0]
     content = [line.rstrip('\n') for line in codecs.open(filename, encoding='utf-8')]
-
+    
     # gather set of unicode characters
     unicodeSetRaw = []
     for line in content:
@@ -219,6 +173,33 @@ for poem in poemCorpus:
 
     print '\n'
 
-    # write data to file - categories
     
-    outputFileName = name + '-dataByCategory.csv'
+
+
+# main variables
+
+poemCorpus = ['NachtUndTraumeIPA.txt', 'WohinIPA.txt', 'DasWandernIPA.txt']
+vowelTypes = ['close', 'closeMid', 'neutral', 'openMid', 'open']
+phonemeCategory = {
+    'a': 'open',
+    u'\u0061': 'open',
+    'e': 'closeMid',
+    u'\u025b': 'openMid',
+    u'\u0259': 'neutral',
+    'i': 'close',
+    'I': 'open',
+#    u'\u026a': 'close',
+    'o': 'closeMid',
+    u'\u0254': 'openMid',
+    u'\u00f8': 'closeMid',
+    u'\u0153': 'openMid',
+    'y': 'close',
+    u'\u0153': 'close',
+    'u': 'close',
+    u'\028a': 'close',
+}
+    
+# run it
+
+for poem in poemCorpus:
+    poemAnalysisByLine(poem, categories=vowelTypes, categoryDict=phonemeCategory, ignoreDiphthongs=True, threshold=2)
