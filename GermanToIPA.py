7677f66873df5b4fcb8ec7414c5efcff521618f3
Kris Shaffer <kris.shaffer@gmail.com>
2015-06-12 09:04:08 -0600

    unicode update to GermanToIPA

--- GermanToIPA.py
+++ GermanToIPA.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
 
+IPADict = {
+    "Heil'ge": 'ha:Il.gə',
+    'Nacht': 'naχt.',
+    'du': 'du.',
+    'sinkest': 'zIŋ.kəst',
+    'nieder': 'ni.dəʁ'
+}
 
+def IPA(poemText, IPADict):
+    IPATranslation = ''
+    return IPATranslation
 
 
 # test
-result = "ha:Il.gə"
-"Heil'ge Nacht, du sinkest nieder;" == result
+print IPA("Heil'ge Nacht, du sinkest nieder;", IPADict) == u"ha:Il.gə naχt. du. zIŋ.kəst ni.dəʁ"
