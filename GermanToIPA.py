e15ffe619328eddc61a4e95d60edb34c05109eb6
Kris Shaffer <kris.shaffer@gmail.com>
2015-07-01 11:59:34 -0600

    added notes to GermanToIPA for future updates

--- GermanToIPA.py
+++ GermanToIPA.py
@@ -8,10 +8,37 @@ IPADict = {
     'nieder': 'ni.dəʁ'
 }
 
-def IPA(poemText, IPADict):
+# replace test dictionary with import-from-CSV function
+
+punctuation = [ ".", ",", ":", ";", "?", "!" "-", "–", "—" ]
+
+
+def IPA(sourceText, IPADict):
     IPATranslation = ''
+    
+    # strip punctuation
+    
+
+    # parse into list of words
+    # http://stackoverflow.com/questions/743806/split-string-into-a-list-in-python
+
+    
+    # replace words with IPA from dictionary (ignoring punctuation)
+
+    
+    # append words (and delimiting spaces) to IPATranslation
+
+    
     return IPATranslation
 
 
+# replace test text with import-from-TXT function
+
+
+
 # test
-print IPA("Heil'ge Nacht, du sinkest nieder;", IPADict) == u"ha:Il.gə naχt. du. zIŋ.kəst ni.dəʁ"
+
+GermanText = "Heil'ge Nacht, du sinkest nieder;"
+IPAOutput = u"ha:Il.gə naχt. du. zIŋ.kəst ni.dəʁ"
+
+print IPA(GermanText, IPADict) == IPAOutput
